be252a3d59e5d4f71b068c7aa5cf3911f3423e2c
Junseok Lee <boris0730@gmail.com>
2021-04-05 21:09:54 +0900

    end

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -14,17 +14,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        priceLabel.text = "₩ \(currentValue)"
+        
+        refresh();
     }
 
     @IBAction func showAlert(_ sender: Any) {
         let message = "가격은 ₩\(currentValue) 입니다"
         
         let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {action in self.refresh()})
         alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-        
+        present(alert, animated: true, completion: nil);
+    }
+    
+    func refresh() {
         let randomPrice = arc4random_uniform(100000) + 1
         currentValue = Int(randomPrice)
         priceLabel.text = "₩ \(currentValue)"
